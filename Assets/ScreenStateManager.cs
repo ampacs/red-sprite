8f8d1e8578346d257b2aed2daf9b4d64a1dbfe17
André Santiago <andrempacsantiago@outlook.com>
2018-04-22 11:32:43 +0100

    Added missing gameplay elementes, gameover, timer and score

---
+++ Assets/ScreenStateManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenStateManager : MonoBehaviour {
+
+    public enum ServerScreenState {
+        Match, Waiting, Title, GameOver 
+    }
+
+    public enum ClientScreenState {
+        Title, Finder, Match, GameOver
+    }
+
+    public static ScreenStateManager instance;
+
+
+
+    public Text[] scoreMeters;
+    public Text timer;
+
+    void Awake () {
+        if (instance == null) {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    void Start () {
+        
+    }
+
+    void Update () {
+        
+    }
+}
